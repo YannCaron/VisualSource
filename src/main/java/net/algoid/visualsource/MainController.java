42cf47c55416c01bad8a50e1ba6390479d2ffde7
cyann <cyann74@gmail.com>
2017-11-26 12:54:21 +0100

    5 instructionPane

--- src/main/java/net/algoid/visualsource/MainController.java
+++ src/main/java/net/algoid/visualsource/MainController.java
@@ -9,7 +9,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import net.algoid.visualsource.shapes.If;
 
 /**
@@ -29,7 +28,9 @@ public class MainController implements Initializable {
         If shape1 = new If(visualSourcePlaceHolder, 10, 10);
         If shape2 = new If(visualSourcePlaceHolder, 200, 10);
         If shape3 = new If(visualSourcePlaceHolder, 400, 10);
-        visualSourcePlaceHolder.getChildren().addAll(shape1, shape2, shape3);
+        If shape4 = new If(visualSourcePlaceHolder, 10, 200);
+        If shape5 = new If(visualSourcePlaceHolder, 200, 200);
+        visualSourcePlaceHolder.getChildren().addAll(shape1, shape2, shape3, shape4, shape5);
     }
 
     public void setData() {
